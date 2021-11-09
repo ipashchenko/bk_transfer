792e64efc954b52ae63016b14e4dba510e7a3e8b
ilya Pashchenko <in4pashchenko@gmail.com>
2021-11-09 10:24:40 +0300

    Update imaging script.

--- scripts/imager_eht.py
+++ scripts/imager_eht.py
@@ -3,30 +3,39 @@ import matplotlib
 matplotlib.use('Agg')
 
 
-def image_in_eht(uvfits, prior_clean_fits, maxit, frac_nchi2_to_stop=0.01, d1='vis', d2=False, d3=False,
+def image_in_eht(uvfits, prior_fits, maxit_first, maxit_iter,
+                 clipfloor=0.01,
+                 frac_nchi2_to_stop=0.01, d1='vis', d2=False, d3=False,
                  alpha_d1=100, alpha_d2=100, alpha_d3=100,
                  s1='simple', s2=False, s3=False,
                  alpha_s1=1, alpha_s2=1, alpha_s3=1,
                  alpha_flux=500, alpha_cm=500, outname=None, pdfname=None, **eht_kwargs):
     """
-       d1 (str): The first data term; options are 'vis', 'bs', 'amp', 'cphase', 'cphase_diag' 'camp', 'logcamp', 'logcamp_diag'
-       d2 (str): The second data term; options are 'vis', 'bs', 'amp', 'cphase', 'cphase_diag' 'camp', 'logcamp', 'logcamp_diag'
-       d3 (str): The third data term; options are 'vis', 'bs', 'amp', 'cphase', 'cphase_diag' 'camp', 'logcamp', 'logcamp_diag'
+    Simple script to image in eht-imaging library.
 
-       s1 (str): The first regularizer; options are 'simple', 'gs', 'tv', 'tv2', 'l1', 'patch','compact','compact2','rgauss'
-       s2 (str): The second regularizer; options are 'simple', 'gs', 'tv', 'tv2','l1', 'patch','compact','compact2','rgauss'
-       s3 (str): The third regularizer; options are 'simple', 'gs', 'tv', 'tv2','l1', 'patch','compact','compact2','rgauss'
+    :param clipfloor:
+        Image pixels of prior image with intensity < clipfloor*PriorImage will
+        be masked using embedding.
 
-       alpha_d1 (float): The first data term weighting
-       alpha_d2 (float): The second data term weighting
-       alpha_d2 (float): The third data term weighting
+    From eht-imaging:
+    d1 (str): The first data term; options are 'vis', 'bs', 'amp', 'cphase', 'cphase_diag' 'camp', 'logcamp', 'logcamp_diag'
+    d2 (str): The second data term; options are 'vis', 'bs', 'amp', 'cphase', 'cphase_diag' 'camp', 'logcamp', 'logcamp_diag'
+    d3 (str): The third data term; options are 'vis', 'bs', 'amp', 'cphase', 'cphase_diag' 'camp', 'logcamp', 'logcamp_diag'
 
-       alpha_s1 (float): The first regularizer term weighting
-       alpha_s2 (float): The second regularizer term weighting
-       alpha_s3 (float): The third regularizer term weighting
+    s1 (str): The first regularizer; options are 'simple', 'gs', 'tv', 'tv2', 'l1', 'patch','compact','compact2','rgauss'
+    s2 (str): The second regularizer; options are 'simple', 'gs', 'tv', 'tv2','l1', 'patch','compact','compact2','rgauss'
+    s3 (str): The third regularizer; options are 'simple', 'gs', 'tv', 'tv2','l1', 'patch','compact','compact2','rgauss'
 
-       alpha_flux (float): The weighting for the total flux constraint
-       alpha_cm (float): The weighting for the center of mass constraint
+    alpha_d1 (float): The first data term weighting
+    alpha_d2 (float): The second data term weighting
+    alpha_d2 (float): The third data term weighting
+
+    alpha_s1 (float): The first regularizer term weighting
+    alpha_s2 (float): The second regularizer term weighting
+    alpha_s3 (float): The third regularizer term weighting
+
+    alpha_flux (float): The weighting for the total flux constraint
+    alpha_cm (float): The weighting for the center of mass constraint
     """
     # Some big value to start with
     previous_chi2_normalized = 1000
@@ -37,8 +46,7 @@ def image_in_eht(uvfits, prior_clean_fits, maxit, frac_nchi2_to_stop=0.01, d1='v
     res = obs.res()
 
     # Setting prior image
-    # Beam-convolved CLEAN image as prior
-    imp = eh.image.load_fits(prior_clean_fits, aipscc=True)
+    imp = eh.image.load_fits(prior_fits, aipscc=False)
     # If aipscc=True => it uses delta pulse function. Hence fix it. Its OK for Prior image.
     imp.pulse = eh.trianglePulse2D
 
@@ -51,7 +59,7 @@ def image_in_eht(uvfits, prior_clean_fits, maxit, frac_nchi2_to_stop=0.01, d1='v
         print("Moving center of mass of the prior image to (0,0)")
         imp = imp.center()
 
-    imp = imp.threshold(cutoff=0.01)
+    imp = imp.threshold(cutoff=clipfloor, fill_val=0.0)
 
     chi2_normalized_cc = obs.chisq(imp, dtype=d1)
     if d2:
@@ -60,14 +68,13 @@ def image_in_eht(uvfits, prior_clean_fits, maxit, frac_nchi2_to_stop=0.01, d1='v
         chi2_normalized_cc += obs.chisq(imp, dtype=d3)
     print("nChi2 for CLEAN model is {:.3f}".format(chi2_normalized_cc))
 
-    # If aipscc=True => need to convolve with beam
     imp = imp.blur_circ(beamparams[0])
 
     # First iteration with CLEAN-image prior
     out = eh.imager_func(obs, imp, imp, imp.total_flux(),
                          d1, d2, d3, alpha_d1, alpha_d2, alpha_d3, s1, s2, s3, alpha_s1, alpha_s2, alpha_s3,
-                         alpha_flux, alpha_cm,
-                         maxit=maxit, ttype='nfft', weighting="natural", **eht_kwargs)
+                         alpha_flux, alpha_cm, clipfloor=clipfloor,
+                         maxit=maxit_first, ttype='nfft', weighting="natural", **eht_kwargs)
 
     current_chi2_normalized = obs.chisq(out, dtype=d1)
     if d2:
@@ -82,8 +89,8 @@ def image_in_eht(uvfits, prior_clean_fits, maxit, frac_nchi2_to_stop=0.01, d1='v
         out = out.blur_circ(res)
         out = eh.imager_func(obs, out, out, imp.total_flux(),
                              d1, d2, d3, alpha_d1, alpha_d2, alpha_d3, s1, s2, s3, alpha_s1, alpha_s2, alpha_s3,
-                             alpha_flux, alpha_cm,
-                             maxit=maxit, ttype='nfft', weighting="natural", **eht_kwargs)
+                             alpha_flux, alpha_cm, clipfloor=clipfloor,
+                             maxit=maxit_iter, ttype='nfft', weighting="natural", **eht_kwargs)
         previous_chi2_normalized = current_chi2_normalized
         current_chi2_normalized = obs.chisq(out, dtype=d1)
         if d2:
@@ -108,7 +115,9 @@ def image_in_eht(uvfits, prior_clean_fits, maxit, frac_nchi2_to_stop=0.01, d1='v
 
 if __name__ == "__main__":
     uvfits = "/home/ilya/data/alpha/MOJAVE/1228+126.u.2020_07_02.uvf"
-    clean_image_tap = "/home/ilya/data/alpha/MOJAVE/1228+126.u.2020_07_02.ict.fits"
-    eht_image = image_in_eht(uvfits, clean_image_tap, maxit=300, frac_nchi2_to_stop=0.03,
+    initial_fits = "/home/ilya/data/alpha/MOJAVE/1228+126_original_stack_I.fits"
+    eht_image = image_in_eht(uvfits, initial_fits, clipfloor=0.01,
+                             maxit_first=300, maxit_iter=100, frac_nchi2_to_stop=0.03,
                              d1="vis", alpha_d1=1,
-                             s1="simple", alpha_s1=1, outname="mojave", pdfname="mojave.pdf")
+                             s1="simple", s2="tv", alpha_s1=10.0, alpha_s2=1.0,
+                             outname="mojave", pdfname="mojave.pdf")
