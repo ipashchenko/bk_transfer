1da1597c6c18a0e9b3aa09328725e8d06991de26
ilya Pashchenko <in4pashchenko@gmail.com>
2023-11-24 03:19:25 +0300

    All flares components should be visible at a single epoch.

--- flare_simulations.py
+++ flare_simulations.py
@@ -241,7 +241,7 @@ for i in range(n_sources):
 
         # First flare
         # t_start_years = np.random.uniform(-1, 1., size=1)[0]
-        t_start_years = -6.
+        t_start_years = -3.
         t_start_days = t_start_years*12*30
         # FIXME:
         amp_N = np.random.uniform(3, 10, size=1)[0]
@@ -282,7 +282,7 @@ for i in range(n_sources):
         # Maximal number of flares
         for i_fl in range(1):
             # Waiting time 2 yrs
-            dt_yrs = 6.0
+            dt_yrs = 3.0
             # while dt_yrs < 2.0:
             #     dt_yrs = np.random.exponential(2.0)
             t_start_years += dt_yrs
